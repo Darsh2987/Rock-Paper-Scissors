2154e39034bc2d7edfa3d69344dad212c590e94f
Darsh <darsh2987@gmail.com>
2018-02-12 19:27:39 +0000

    addeded functions to display results for each game round and end of game

--- app.js
+++ app.js
@@ -3,16 +3,38 @@ document.querySelector("h2").addEventListener("click", game);
 
 function game() {
 
+  // Setting game scores and round.
   let playerScore = 0;
   let computerScore = 0;
   let draw = 0;
-  let gameCount = 0;
+  let gameCount = 1;
+
+  // Creates the Section element with UL elements inside and appends to body.
+  const section = document.createElement("section");
+  section.className = "result-section"
+  document.body.appendChild(section);
+
+  const roundResults = document.createElement("ul");
+  roundResults.className = "round-results";
+  section.appendChild(roundResults);
+
+  // Player Selection function with validation if player input is not either rock, paper or scissors.
+  function pSelection() {
+    const selection = prompt('Type Rock, Paper or Scissors').toLowerCase();
+
+    if (selection !== 'rock' && selection !== 'paper' && selection !== 'scissors') {
+        console.log('Invalid selection, please type either "Rock", "Paper", or "Scissors"');
+        pSelection();
+      } else {
+      return selection;
+    };
+  };
 
   /**
   * This generates a move from the CPU
   * @return {string} the CPU's choice
   */
-  function computerSelection() {
+  function cSelection() {
     var number = Math.floor(Math.random() * 3) + 1;
 
     if (number === 1) {
@@ -26,73 +48,143 @@ function game() {
 
   /**
   * This is the main function which takes 2 string values and compares them.
+  * Function also displays the results of each game round.
   * @param {string} player
   * @param {string} computer
   */
-  function playRound(player, computer) {
+  function playRound(playerSelection, computerSelection) {
 
-    console.log(`computer chose ${computer}`);
-    console.log(`player chose ${player}`);
+    const gameRound = document.createElement("ul");
+    gameRound.className = "gameRound";
+    roundResults.appendChild(gameRound);
+
+    const roundNum = `Game round ${gameCount}`;
+    const roundNumLi = document.createElement("li");
+    roundNumLi.className = "game-round";
+    roundNumLi.textContent = roundNum;
+    gameRound.appendChild(roundNumLi);
+
+    const playerChoice = `You chose ${playerSelection}`;
+    const playerLi = document.createElement("li");
+    playerLi.className = "Players-choice";
+    playerLi.textContent = playerChoice;
+    gameRound.appendChild(playerLi);
+
+    const computerChoice = `The computer chose ${computerSelection}`;
+    const computerLi = document.createElement("li");
+    computerLi.className = "computers-choice";
+    computerLi.textContent = computerChoice;
+    gameRound.appendChild(computerLi);
+
+    const roundOutcomeLi = document.createElement("li");
+    roundOutcomeLi.className = "round-outcome";
+    gameRound.appendChild(roundOutcomeLi);
 
     switch(true) {
-      case player === "rock" && computer === "paper":
+      case playerSelection === "rock" && computerSelection === "paper":
         console.log("You Lose! Paper beats Rock");
+        roundOutcomeLi.textContent = "You Lose this round! Paper beats Rock";
         computerScore++;
         break;
-      case player === "rock" && computer === "scissors":
+      case playerSelection === "rock" && computerSelection === "scissors":
         console.log("You Win! Rock beats Scissors");
+        roundOutcomeLi.textContent = "You Win this round! Rock beats Scissors";
         playerScore++;
         break;
-      case player === "paper" && computer === "rock":
+      case playerSelection === "paper" && computerSelection === "rock":
         console.log("You Win! Paper beats Rock");
+        roundOutcomeLi.textContent = "You Win this round! Paper beats Rock";
         playerScore++;
         break;
-      case player === "paper" && computer === "scissors":
+      case playerSelection === "paper" && computerSelection === "scissors":
         console.log("You Lose! Scissors beats Paper");
+        roundOutcomeLi.textContent = "You Lose this round! Scissors beats Paper";
         computerScore++;
         break;
-      case player === "scissors" && computer === "rock":
+      case playerSelection === "scissors" && computerSelection === "rock":
         console.log("You Lose! Rock beats Scissors");
+        roundOutcomeLi.textContent = "You Lose this round! Rock beats Scissors";
         computerScore++;
         break;
-      case player === "scissors" && computer === "paper":
+      case playerSelection === "scissors" && computerSelection === "paper":
         console.log("You Win! Scissors beats Paper");
+        roundOutcomeLi.textContent = "You Win this round! Scissors beats Paper";
         playerScore++;
         break;
-      case player === computer:
+      case playerSelection === computerSelection:
         console.log("Draw!");
+        roundOutcomeLi.textContent = "You both chose the same item, your both loser's";
         draw++;
+    }
+
+    const player_Score = `Your score = ${playerScore}`;
+    const player_ScoreLi = document.createElement("li");
+    player_ScoreLi.className = "players-score";
+    player_ScoreLi.textContent = player_Score;
+    gameRound.appendChild(player_ScoreLi);
+
+    const computer_Score = `The computer Score = ${computerScore}`;
+    const computer_ScoreLi = document.createElement("li");
+    computer_ScoreLi.className = "computers-score";
+    computer_ScoreLi.textContent = computer_Score;
+    gameRound.appendChild(computer_ScoreLi)
+
+    const draw_Score = `Draws = ${draw}`;
+    const draw_ScoreLi = document.createElement("li");
+    draw_ScoreLi.className = "draw";
+    draw_ScoreLi.textContent = draw_Score;
+    gameRound.appendChild(draw_ScoreLi);
+  };
+
+  // Function to display total results at the end of the 5 rounds.
+  function totalResults() {
+
+    const totalResults = document.createElement("ul");
+    totalResults.className = "total-results";
+    section.appendChild(totalResults);
+
+    const totalPlayerScore = `Your total score ${playerScore}`;
+    const totalPlayerScoreLi = document.createElement("li");
+    totalPlayerScoreLi.className = "total-player-score";
+    totalPlayerScoreLi.textContent = totalPlayerScore;
+    totalResults.appendChild(totalPlayerScoreLi);
+
+    const totalComputerScore = `Computers total score ${computerScore}`;
+    const totalComputerScoreLi = document.createElement("li");
+    totalComputerScoreLi.className = "total-computer-score";
+    totalComputerScoreLi.textContent = totalComputerScore;
+    totalResults.appendChild(totalComputerScoreLi);
+
+    const totalDraws = `Total draws ${draw}`;
+    const totalDrawsLi = document.createElement("li");
+    totalDrawsLi.className = "total-draws";
+    totalDrawsLi.textContent = totalDraws;
+    totalResults.appendChild(totalDrawsLi);
+  };
+
+  // Function to display end game result wherever the player or computer has won or lost.
+  function endResult() {
+    const endResult = document.createElement("h2");
+    endResult.className = "end-result";
+    section.appendChild(endResult);
+
+    if (playerScore < computerScore) {
+      endResult.textContent = "You lose, better luck next time!";
+    } else if (playerScore > computerScore) {
+      endResult.textContent = "You win, Congratualations!";
+    } else {
+      endResult.textContent = "It's a tie, no one wins!"
     }
   };
 
-  // Player Selection function with validation if player input is not either rock, paper or scissors.
-  function playerSelection() {
-    const selection = prompt('Type Rock, Paper or Scissors').toLowerCase();
-
-    if (selection !== 'rock' && selection !== 'paper' && selection !== 'scissors') {
-        console.log('Invalid selection, please type either "Rock", "Paper", or "Scissors"');
-        playerSelection();
-      } else {
-      return selection;
-    };
-  };
-
-
-
   // While loop to play 5 round of the game.
-  while(gameCount < 5) {
-    const player = playerSelection();
-    const computer = computerSelection();
-    playRound(player, computer);
+  while(gameCount < 6) {
+    const playerSelection = pSelection();
+    const computerSelection = cSelection();
+    playRound(playerSelection, computerSelection);
     gameCount++
-
-    console.log("player score = " + playerScore);
-    console.log("computer score = " + computerScore);
-    console.log("draws " + draw);
-    console.log("game round " + gameCount + '\n');
   };
-  console.log("total player score = " + playerScore);
-  console.log("total computer score = " + computerScore);
-  console.log("total draws " + draw);
-  console.log("total game round " + gameCount + '\n');
+
+  totalResults();
+  endResult();
 };
